df73e89ae3c2725c170a3a239315fca49251ff1d
Havilah Uche-Aniche <105943706+havilah-dalalord@users.noreply.github.com>
2023-12-27 14:49:06 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
+
+
+// const heading = document.querySelector(".card-title")
+// // console.log(heading.textContent)
+// heading.innerHTML = 'Hello World'
+// console.log(heading)
